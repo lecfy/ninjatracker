92b73b424d0d3c5ea39d4554f09569aea1c3895b
ninjachicks <carolin.bruederle@gmail.com>
2021-08-26 20:32:41 +0200

    change class MyApp to NinjaTracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(NinjaTracker());
 }
 
-class MyApp extends StatelessWidget {
+class NinjaTracker extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
