78b03e09bd2bc24d398fc1dec231018455e7896c
ninjachicks <carolin.bruederle@gmail.com>
2021-08-26 20:49:23 +0200

    add second screen and toggle button

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(NinjaTracker());
+  runApp(MaterialApp(
+    title: 'Ninjatracker',
+    home: HomeScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class NinjaTracker extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ninjatracker'),
       ),
-      home: MyHomePage(title: 'Ninjatracker'),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Login'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Login()),
+            );
+        },
+        ),
+      ),
     );
   }
 }
 
+class Login extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Back'),
+          onPressed: () {
+            Navigator.pop(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+/*
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -112,3 +139,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
